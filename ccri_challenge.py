c3f91d3075a9ddb0853d7f935b95e59cc6aa43d0
Cynthia <cynthiaukawu@gmail.com>
2023-04-08 17:53:31 -0500

    working version with more test cases; I'm using a list instead of a dictionary

--- ccri_challenge.py
+++ ccri_challenge.py
@@ -29,7 +29,6 @@ So we're getting a list of unique values near a positive number?
 Ideas
 * Need to find positive value and iterate from there
 * What if all the values are negative?
-* What if the matrix is 2x2, the smallest matrix would be 2x2 correct?
 * Implement a cache so that values don't get recomputed
     - replace neighbors = [] with a dictionary. Lookup would be constant time rather than linear
 
@@ -59,128 +58,64 @@ example2: if you go up three, you can either go left zero times or right zero ti
         [-16🚫,    -17✅,    -18✅,  -19✅,  -20🚫]
 ]
 """
-
-# def bfs_recursive_n_steps(matrix, queue, visited, steps):
-#     if not queue:
-#         return
-
-#     print("matrix", matrix)
-#     print("queue", queue)
-#     print("steps", steps)
-#     current = queue.pop(0)
-#     visited.add(current)
-#     print("current", current)
-#     print("visited", visited)
-
-#     # Process current node
-#     print(matrix[current[0]][current[1]])
-#     print()
-
-#     # Add neighbors to the queue
-#     neighbors = get_neighbors(matrix, current, steps)
-#     print("neighbors", neighbors)
-#     for neighbor in neighbors:
-#         if neighbor not in visited:
-#             queue.append(neighbor)
-
-#     # Recursively call bfs on the next node in the queue
-#     bfs_recursive_n_steps(matrix, queue, visited, steps-1)
-
-
 import time
 
 
 def search_2d_array(matrix, start, steps):
     if len(start) < 1:
-        return "Empty"
+        print("Empty")
+        return 0
 
     visited = set()
     for value in start:
         neighbors = get_neighbors(matrix, value, steps)
-        visited.update(neighbors.keys())
-    # start = start[0]
-    # neighbors = get_neighbors(matrix, start, steps)
-    return sorted(visited)
+        visited.update(neighbors)
+    return len(visited)
 
 
+# Method that will find all the neighbors to the starting points
 def get_neighbors(matrix, node, steps):
-    neighbors = dict()
+    neighbors = []
     row, col = node[0], node[1]
-    rows, cols = len(matrix), len(matrix[0])
+    # rows, cols = len(matrix), len(matrix[0])
+    rows = len(matrix)
 
+    # algorithm to move around the matrix
     for i in range(steps+1):
-        if row-i or row+i == row:
+        if i > rows-1:  # takes care of large steps in the vertical direction
+            return neighbors
+        if row-i or row+i == row:  # stay on the same row
             if col-i >= 0:
-                # Go Left
-                if (row, col - i) not in neighbors:
-                    neighbors[(row, col - i)] = 1
-                else:
-                    neighbors[(row, col - i)] += 1
-            if col+i <= cols - 1:
-                # Go Right
-                if (row, col + i) not in neighbors:
-                    neighbors[(row, col + i)] = 1
-                else:
-                    neighbors[(row, col + i)] += 1
-        if row-i >= 0:
-            # Go Up
-            if (row - i, col) not in neighbors:
-                neighbors[(row - i, col)] = 1
-            else:
-                neighbors[(row - i, col)] += 1
+                neighbors.append((row, col - i))  # Left
+            if col+i <= len(matrix[i]) - 1:
+                neighbors.append((row, col + i))  # Right
+        if row-i >= 0:  # Move up
+            if col < len(matrix[row-i]):
+                neighbors.append((row - i, col))
             k = steps-i
             while k >= 0:
-                if col-k >= 0:
-                    # Left
-                    if (row-i, col - k) not in neighbors:
-                        neighbors[(row-i, col - k)] = 1
-                    else:
-                        neighbors[(row-i, col - k)] += 1
-                if col+k <= cols - 1:
-                    # Right
-                    if (row-i, col + k) not in neighbors:
-                        neighbors[(row-i, col + k)] = 1
-                    else:
-                        neighbors[(row-i, col + k)] += 1
+                if col-k >= 0 and col-k < len(matrix[row-i]):
+                    neighbors.append((row - i, col - k))  # Left
+                if col+k < len(matrix[row-i]):
+                    neighbors.append((row - i, col + k))  # Right
                 k -= 1
-        if row+i <= rows - 1:
-            # Go Down
-            if (row + i, col) not in neighbors:
-                neighbors[(row + i, col)] = 1
-            else:
-                neighbors[(row + i, col)] += 1
+        if row+i <= rows - 1:  # Move down
+            if col < len(matrix[row+i]):
+                neighbors.append((row + i, col))
             k = steps-i
             while k >= 0:
-                if col-k >= 0:
-                    # Left
-                    if (row + i, col - k) not in neighbors:
-                        neighbors[(row + i, col - k)] = 1
-                    else:
-                        neighbors[(row + i, col - k)] += 1
-                if col+k <= cols - 1:
-                    # Right
-                    if (row + i, col + k)not in neighbors:
-                        neighbors[(row + i, col + k)] = 1
-                    else:
-                        neighbors[(row + i, col + k)] += 1
+                # make sure the cell exists if the array is jagged
+                if col-k >= 0 and col-k < len(matrix[row+i]):
+                    neighbors.append((row + i, col - k))  # Left
+                if col+k < len(matrix[row+i]):
+                    neighbors.append((row + i, col + k))  # Right
                 k -= 1
     return neighbors
 
 
+# Method that will find all the positive starting points
 def find_positive_values(matrix):
     max_row = len(matrix)
-    max_col = len(matrix[0])
     positives = [(i, k) for i in range(max_row)
-                 for k in range(max_col) if matrix[i][k] > 0]
+                 for k in range(len(matrix[i])) if matrix[i][k] > 0]
     return positives
-
-
-test = [
-    [-1, -2, -3, -4, -5],
-    [-6,  -7, -8,  9, -10],
-    [11, -12, -13, -14, -15],
-    [-16, -17, -18, -19, -20]
-]
-
-starting_points = find_positive_values(test)
-print(search_2d_array(test, starting_points, 3))
--- test_code.py
+++ test_code.py
@@ -1,5 +1,6 @@
 import pytest
 import ccri_challenge
+import numpy as np
 
 
 @pytest.fixture()
@@ -11,14 +12,16 @@ def matrix():
         [-16, -17, -18, -19, -20]
     ]
 
-# @pytest.fixture()
-# def matrix2():
-#     return [
-#         [-1, -2, -3, -4, -5],
-#         [-6,  -7,   8,  -9, -10],
-#         [-11, -12, -13, -14, -15],
-#         [-16, -17, -18, -19, - 20]
-#     ]
+
+@pytest.fixture()
+def matrix2():
+    return [
+        [-1, -2, -3, -4, -5, -6, -7],
+        [-8, -9, -10, -11, -12, -13, -14],
+        [-15, -16, -17, -18, -19, -20, -33],
+        [-21, -22, -23, -24, 25, -26, -34],
+        [-27, -28, -29, -30, -31, -32, -35]
+    ]
 
 
 @pytest.fixture()
@@ -29,6 +32,54 @@ def matrix3():
         [11, 12, 13, -14, -15],
         [16, 17, 18, -19, -20]
     ]
+
+
+@pytest.fixture()
+def matrix4():
+    return [
+        [3]
+    ]
+
+
+@pytest.fixture()
+def matrix5():
+    return [
+        [-3]
+    ]
+
+
+@pytest.fixture()
+def matrix6():
+    return [
+        [3, -1]
+    ]
+
+
+@pytest.fixture()
+def matrix7():
+    return [
+        [3],
+        [0]
+    ]
+
+
+@pytest.fixture()
+def jagged_array():
+    return [
+        [-1, -2, -3, -4, -5, -6, -7],
+        [-8, -9, -10, -11, -12, -13, -14],
+        [-15, -16, -17, -18, -19, -20, -33, 18, -20, -5],
+        [-21, -22, -23, -24, 25, -26, -34],
+        [-27, -28, -29, -30, -31, -32, -35]
+    ]
+
+
+@pytest.fixture()
+def large_array():
+    arr = np.full((11, 11), -1)
+    arr[6, 5] = 1
+    arr[7, 3] = 5
+    return arr
 
 
 def test_find_positive_values(matrix):
@@ -52,27 +103,85 @@ def test_search_2d_array_N_equals_3(matrix):
     coordinates = ccri_challenge.find_positive_values(matrix)
 
     # N = 3
-    matches = sorted(ccri_challenge.search_2d_array(matrix, coordinates, 3))
+    matches = ccri_challenge.search_2d_array(matrix, coordinates, 3)
     correct = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (1, 1), (1, 2), (1, 3),
                (1, 4), (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (3, 0), (3, 1), (3, 2), (3, 3), (3, 4)]
-    assert matches == correct
+    assert matches == len(correct)
 
 
 def test_search_2d_array_N_equals_6(matrix):
     coordinates = ccri_challenge.find_positive_values(matrix)
 
     # N = 6
-    matches = sorted(ccri_challenge.search_2d_array(matrix, coordinates, 6))
+    matches = ccri_challenge.search_2d_array(matrix, coordinates, 6)
     correct = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (1, 1), (1, 2), (1, 3),
                (1, 4), (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (3, 0), (3, 1), (3, 2), (3, 3), (3, 4)]
-    assert matches == correct
+    assert matches == len(correct)
 
 
 def test_matrix3_N_equals_3(matrix3):
     coordinates = ccri_challenge.find_positive_values(matrix3)
 
     # N = 3
-    matches = sorted(ccri_challenge.search_2d_array(matrix3, coordinates, 3))
+    matches = ccri_challenge.search_2d_array(matrix3, coordinates, 3)
     correct = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (1, 1), (1, 2), (1, 3),
                (1, 4), (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (3, 0), (3, 1), (3, 2), (3, 3), (3, 4)]
-    assert matches == correct
+    assert matches == len(correct)
+
+
+def test_matrix2_N_equals_5(matrix2):
+    coordinates = ccri_challenge.find_positive_values(matrix2)
+    matches = ccri_challenge.search_2d_array(matrix2, coordinates, 5)
+    correct = [
+        (0, 2), (0, 3), (0, 4), (0, 5), (0, 6),
+        (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6),
+        (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6),
+        (3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6),
+        (4, 0), (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6)
+    ]
+    assert matches == len(correct)
+
+
+def test_1_element(matrix4):
+    coordinates = ccri_challenge.find_positive_values(matrix4)
+    matches = ccri_challenge.search_2d_array(matrix4, coordinates, 100)
+    correct = [(0, 0)]
+    assert matches == len(correct)
+
+
+def test_no_positives(matrix5):
+    coordinates = ccri_challenge.find_positive_values(matrix5)
+    matches = ccri_challenge.search_2d_array(matrix5, coordinates, 8)
+    assert matches == 0
+
+
+def test_single_row(matrix6):
+    coordinates = ccri_challenge.find_positive_values(matrix6)
+    matches = ccri_challenge.search_2d_array(matrix6, coordinates, 100)
+    correct = [(0, 0), (0, 1)]
+    assert matches == len(correct)
+
+
+def test_single_column(matrix7):
+    coordinates = ccri_challenge.find_positive_values(matrix7)
+    matches = ccri_challenge.search_2d_array(matrix7, coordinates, 20)
+    correct = [(0, 0), (1, 0)]
+    assert matches == len(correct)
+
+
+def test_jagged_array_N_equals_0(jagged_array):
+    coordinates = ccri_challenge.find_positive_values(jagged_array)
+    matches = ccri_challenge.search_2d_array(jagged_array, coordinates, 0)
+    assert matches == 2
+
+
+def test_jagged_array_N_equals_4(jagged_array):
+    coordinates = ccri_challenge.find_positive_values(jagged_array)
+    matches = ccri_challenge.search_2d_array(jagged_array, coordinates, 4)
+    assert matches == 31
+
+
+def test_11x11_array(large_array):
+    coordinates = ccri_challenge.find_positive_values(large_array)
+    matches = ccri_challenge.search_2d_array(large_array, coordinates, 2)
+    assert matches == 22
